903bd69b46d783ddc40b13a87bf70b4d86d0c903
Kartikay Singh <kartikay.singh.0211@gmail.com>
2025-08-25 23:54:31 +0530

    partners link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -61,7 +61,7 @@ const Footer = () => {
 							</li>
 							<li>
 								<Link
-									href={"/partner-moto"}
+									href={"/partners"}
 									className="text-gray-300 hover:text-green-400 transition-colors duration-200 cursor-pointer"
 								>
 									Partners
